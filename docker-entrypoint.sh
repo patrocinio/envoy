c46d53d892e4bc82e728ffa498f83041dda64062
patrocinio <epatro@gmail.com>
2019-10-15 20:22:45 -0400

    Expose port number

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$SERVICE_NAME > /etc/envoy.yaml
+cat /tmpl/envoy.yaml.tmpl | envsubst \$SERVICE_NAME,\$PORT_NUMBER > /etc/envoy.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
